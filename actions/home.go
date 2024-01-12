a82baeb255e9efd551ba82bf663a4163510a6335
danielgottschalck <znowm4n@gmail.com>
2024-01-12 13:42:46 +0100

    Update

--- actions/home.go
+++ actions/home.go
@@ -4,12 +4,15 @@ import (
 	"net/http"
 
 	"github.com/gobuffalo/buffalo"
+
+	"znowgo/models"
 )
 
 // HomeHandler is a default handler to serve up
 // a home page.
 func HomeHandler(c buffalo.Context) error {
 	c.Set("current_user_id", c.Session().Get("current_user_id"))
+	c.Set("user", models.User{})
 
 	return c.Render(http.StatusOK, r.HTML("home/index.plush.html"))
 }
