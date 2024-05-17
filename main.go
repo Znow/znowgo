426597e49aea9533b9b01d99abfba3165bc0f7dd
Daniel <daniel.gottschalck@lego.com>
2024-05-17 19:28:49 +0200

    Added GORM

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"server/server/database"
+	"server/server/model"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	loadEnv()
+	loadDatabase()
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
@@ -15,4 +22,17 @@ func main() {
 	})
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func loadDatabase() {
+	database.Connect()
+	database.Database.AutoMigrate(&model.User{})
+	database.Database.AutoMigrate(&model.Entry{})
+}
+
+func loadEnv() {
+	err := godotenv.Load(".env.local")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 }
